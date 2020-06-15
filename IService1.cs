855fefb344db331c6d42b13065091d23e2760e4f
SriKrishnaChaitanyan <66622103+SriKrishnaChaitanyan@users.noreply.github.com>
2020-06-15 12:09:32 +0530

    Added WCF Service Files

---
+++ IService1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace EmployeeWCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        IEnumerable<Employee> GetEmployees();
+
+        [OperationContract]
+        void InsertEmployee(Employee emp);
+
+        [OperationContract]
+        void UpdateEmployee(Employee emp);
+
+        [OperationContract]
+        void DeleteEmployee(int empId);
+
+        
+    }
+
+    [DataContract]
+    public class Employee
+    {
+        [DataMember]
+        [Key]
+        [Required]
+        public int EmployeeId { get; set; }
+        
+        [DataMember]
+        [Required]
+        public string EmployeeName { get; set; }
+
+        [DataMember]
+        [Required]
+        public int EmployeeSal { get; set; }
+
+        [DataMember]
+        public string Street { get; set; }
+
+        [DataMember]
+        public string City { get; set; }
+
+        [DataMember]
+        public string State { get; set; }
+
+        [DataMember]
+        [Required]
+        public string EmployeeAddress { get; set;  }
+
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EmployeeWCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public void DeleteEmployee(int empId)
+        {
+            EmployeeDBModel emp = new EmployeeDBModel();
+
+            var e = (from obj in emp.Employees
+                     where obj.EmployeeId == empId
+                     select obj).First();
+
+            emp.Employees.Remove(e);
+            emp.SaveChanges();
+
+        }
+
+        public IEnumerable<Employee> GetEmployees()
+        {
+            List<Employee> emp = new List<Employee>();
+            EmployeeDBModel obj = new EmployeeDBModel();
+            emp = obj.Employees.ToList();
+
+            return emp;
+
+        }
+
+        public void InsertEmployee(Employee emp)
+        {
+            EmployeeDBModel obj = new EmployeeDBModel();
+            obj.Employees.Add(emp);
+            obj.SaveChanges();
+
+        }
+
+        public void UpdateEmployee(Employee emp)
+        {
+            EmployeeDBModel obj = new EmployeeDBModel();
+            var c = (from e in obj.Employees
+                     where e.EmployeeId == emp.EmployeeId
+                     select e).First();
+
+            c.EmployeeName = emp.EmployeeName;
+            c.EmployeeSal = emp.EmployeeSal;
+            c.EmployeeAddress = emp.EmployeeAddress;
+            c.Street = emp.Street;
+            c.City = emp.City;
+            c.State = emp.State;
+
+            obj.SaveChanges();
+
+        }
+    }
+}
